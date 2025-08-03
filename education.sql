159b622f09a496d9909f0224f455409b046a0829
Md. Humayun Farid <humayunbee@gmail.com>
2025-08-03 09:59:36 +0600

    Update education.sql with additional SQL queries and comments

--- education.sql
+++ education.sql
@@ -20,3 +20,49 @@ WHERE stu_id = 64
 UPDATE `students`
 SET `f_phone` = CONCAT('0', `f_phone`)
 WHERE LENGTH(`f_phone`) = 10;
+
+
+
+laravel education_management
+
+
+-- Database add colum
+
+ALTER TABLE `system_settings` ADD `theme` int NOT NULL DEFAULT 1 AFTER `website`;
+
+ALTER TABLE `subjects` ADD `subject_group` VARCHAR(30) NULL DEFAULT NULL AFTER `section_id`;
+
+-- তুমি যদি চাও যেসব marks এন্ট্রির subject_id আর subjects টেবিলে নেই (মানে ডিলিট হয়ে গেছে), সেগুলো মুছে ফেলতে — তাহলে নিচের DELETE কুয়েরি ব্যবহার করো:
+DELETE m
+FROM marks m
+LEFT JOIN subjects s ON m.subject_id = s.id
+WHERE s.id IS NULL;
+
+
+
+
+
+-- ✅ ভুল subject যুক্ত মার্ক এন্ট্রি (সার্চ):
+
+SELECT m.*
+FROM marks m
+JOIN student_details sd ON m.stu_details_id = sd.id
+LEFT JOIN subjects s 
+    ON s.group_id = sd.group_id 
+    AND s.class_id = sd.class_id 
+    AND s.medium_id = sd.medium_id 
+    AND m.subject_id = s.id
+WHERE s.id IS NULL;
+
+
+-- এই রেজাল্ট দেখে নিশ্চয়তা পেলে পরে নিচের মতো করে ডিলিট করতে পারো:
+
+DELETE m
+FROM marks m
+JOIN student_details sd ON m.stu_details_id = sd.id
+LEFT JOIN subjects s 
+    ON s.group_id = sd.group_id 
+    AND s.class_id = sd.class_id 
+    AND s.medium_id = sd.medium_id 
+    AND m.subject_id = s.id
+WHERE s.id IS NULL;
